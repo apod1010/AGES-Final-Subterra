94478fca40866f3a8a99add4fc4eba0d5177cc02
apod1010 <32077837+apod1010@users.noreply.github.com>
2018-04-04 17:54:56 -0500

    Camera Changes (must be fixed)

---
+++ Assets/Scripts/MenuScripts/TitleScreenManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour {
+
+    public string StartButton = "";
+    public string CreditsButton = "";
+
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene(StartButton);
+    }
+
+    public void OnCreditsButtonClick()
+    {
+        SceneManager.LoadScene(CreditsButton);
+    }
+
+    public void OnQuitButtonClick()
+    {
+        Application.Quit();
+    }
+}
